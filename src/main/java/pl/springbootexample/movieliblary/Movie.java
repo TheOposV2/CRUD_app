24f998fa12fc0f5a747ca08fe996df6c2b65f6fb
TheOposV2 <wojtek.kinczyk@gmail.com>
2023-10-30 11:50:54 +0100

    First build

---
+++ src/main/java/pl/springbootexample/movieliblary/Movie.java
@@ -0,0 +1,16 @@
+package pl.springbootexample.movieliblary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private  int id;
+    private String name;
+    private int rating;
+
+
+}
